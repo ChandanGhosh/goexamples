9e55e14e8f8ac5caa73541268f119f4f7e748862
chandanghosh <ckghosh1983@gmail.com>
2021-06-06 13:10:25 +0530

    added comments for nested functions for easier understandings

--- ex-nested-func/main.go
+++ ex-nested-func/main.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+// “A closure is a nested function that
+// has access to the variables of its parent function,
+// even after the parent has executed.”
+// so essentially the anonymous function
+// gets a new copy of the nested function's
+// state variable i and increment it and
+// nested function store it in memory until
+// the program exists
+// This is why everytime there is a call to
+// nested function would return
+// a newer incremented value of i
+// as every call to nested function
+// would automatically execute the anonymous function
+// inside it
 func nested() func() int {
 	var x int
 	return func() int {
