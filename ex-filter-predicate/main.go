1d4fa4992a403142b057de21ae1068002d2a695b
Chandan Ghosh <ckghosh1983@gmail.com>
2021-11-22 21:51:38 +0530

    example of predicate function added

---
+++ ex-filter-predicate/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+// filter returns a slice with only the values that pred(val) returned true
+func filter(pred func(int) bool, values []int) []int {
+	// add code
+	var result []int
+	for _, v := range values {
+		if pred(v) == true {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func isOdd(num int) bool {
+	return num%2 == 1
+}
+
+func main() {
+	values := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(filter(isOdd, values))
+}
