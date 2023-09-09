66afebf8e39ef8befd425da43ee17f4791c9947b
Chandan Ghosh <ckghosh1983@gmail.com>
2023-09-09 10:11:16 +0530

    function options pattern

---
+++ ex-func-options-pattern/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type Opts struct {
+	maxConn int
+	id      string
+	tls     bool
+}
+
+type OptsFunc func(*Opts)
+
+func DefaultOpts() Opts {
+	return Opts{
+		maxConn: 10,
+		id:      "default",
+		tls:     false,
+	}
+}
+
+type server struct {
+	Opts
+}
+
+func NewServer(opts ...OptsFunc) *server {
+	o := DefaultOpts()
+	for _, fn := range opts {
+		fn(&o)
+	}
+	return &server{
+		Opts: o,
+	}
+}
+
+func getMaxConn(n int) OptsFunc {
+	return func(o *Opts) {
+		o.maxConn = n
+	}
+}
+
+func withTLS(o *Opts) {
+	o.tls = true
+}
+
+func main() {
+	server := NewServer(withTLS, getMaxConn(100))
+	fmt.Printf("%+v\n", server)
+}
